3b7573a3d94a684070dfc74f712ade2e0fa5fde8
PutkisDude <putkis@gmail.com>
2022-02-10 14:45:48 +0200

    shiet

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(require('./record'));
+app.use(require('./user/session'))
 
 // Global error handling.
 app.use(function (err, _req, res) {
