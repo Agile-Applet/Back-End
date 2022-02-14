5853a9f2c8f73161e5fee23ca757eeae742230b7
PutkisDude <putkis@gmail.com>
2022-02-14 11:12:32 +0200

    login checkkaa onko joku jo kirjautunut

--- user/auth.js
+++ user/auth.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const bcryptjs = require("bcryptjs");
-const session = require('express-session');
 const SALT = process.env.SALT; // Bcrypt rounds
 const {check, validationResult} = require('express-validator');
 
@@ -10,39 +9,49 @@ const router = express.Router();
 // Connect to database
 const dbo = require('../conn');
 
-
 // Login
-// TODO: tarkistus onko koneella joku kirjautunut jo
 // TODO: tarkistus onko käyttäjä bannitty
 router.post('/login', async (req, res) => {
-    const {username, password} = req.body;
 
     const dbConnect = dbo.getDb();
+    const {username, password} = req.body;
     const query = { username: username };
-    
-    await dbConnect.collection('players').findOne(query, (err, result) => {
-        if(err) res.status(401).json('You shall not pass'); // error
-        let user = result;
-          if(!user){ 
-              return res.status(401).json('You shall not pass!'); // user doesn't exist
-          }else{
-            const isMatch = bcryptjs.compare(password, user.password);
-            if(!isMatch) res.status(401).json('You shall not pass') // Password mismatch
-        
-            req.session.username = username
-            return res.status(200).json("Logged in")
-          }
 
-    })
+    if(req.session.username){
+        res.status(401).json('Already logged in')
+    }else{
+        await dbConnect.collection('players').findOne(query, (err, result) => {
+            if(err) res.status(401).json('You shall not pass'); // error
+            let user = result;
+              if(!user){ 
+                  return res.status(401).json('You shall not pass!'); // user doesn't exist
+              }else{
+                const isMatch = bcryptjs.compare(password, user.password);
+                if(!isMatch) res.status(401).json('You shall not pass'); // Password mismatch
+            
+                req.session.username = username;
+                req.session.isLogged = true;
+                isAdmin = user.admin || false;
+                return res.status(200).json("Logged in");
+              }
+        })
+    }
 })
 
+router.post('/isLogged', (req, res) => {
+    if(req.session.isLogged) {
+        res.status(200).json(true)
+    }else {
+        res.status(401).json(false);
+    }
+})
 
 // Logout
 router.delete('/logout', (req, res) => {
     if (req.session) {
       req.session.destroy(err => {
         if (err) {
-          res.status(400).send('Unable to log out')
+          res.status(400).json('Unable to log out')
         } else {
           res.send('Logout successful')
         }
@@ -52,23 +61,13 @@ router.delete('/logout', (req, res) => {
     }
   })
 
-router.get('/tester', (req, res) => {
-    if(req.session) {
-        console.log(req.session)
-        res.status(200).send('yee');
-    }else{
-        res.send('yes')
-    }
-})
-
-
-
 // TODO: Add function to check if player or email exists
+// TODO: Add validations
 router.post('/register', async (req, res ) => {
 
     const dbConnect = dbo.getDb();
     const {username, email, password } = req.body;
-  
+
     const salt = bcryptjs.genSaltSync(parseInt(SALT));
     const hashedPassword = bcryptjs.hashSync(password, salt);
 
@@ -91,5 +90,14 @@ router.post('/register', async (req, res ) => {
         });
     }
 )
+
+router.get('/tester', (req, res) => {
+    if(req.session) {
+        console.log(req.session);
+        res.status(200).json('yee');
+    }else{
+        res.json('yes')
+    }
+})
 
 module.exports = router;
