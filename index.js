34aeb955a2ba6781a61b6e1d56b8599e30aac997
Otso Metsäportti <62076266+otsojm@users.noreply.github.com>
2022-04-19 09:51:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("express")();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http, { cors: { origin: '*' },}); 
+const io = require("socket.io")(http); 
 const express = require("express");
 const cors = require("cors");
 const dbo = require("./infrastructure/conn");
